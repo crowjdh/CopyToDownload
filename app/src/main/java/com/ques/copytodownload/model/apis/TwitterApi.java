ecdd40eabf8187cd187129c6d780186281800a17
ChrisJeong <crowjdh@gmail.com>
2018-05-25 11:56:39 +0900

    Remove hard-coded twitter api key and secret

--- app/src/main/java/com/ques/copytodownload/model/apis/TwitterApi.java
+++ app/src/main/java/com/ques/copytodownload/model/apis/TwitterApi.java
@@ -2,9 +2,14 @@ package com.ques.copytodownload.model.apis;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
+import com.ques.copytodownload.BuildConfig;
+import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.services.StatusesService;
@@ -27,8 +32,13 @@ public class TwitterApi {
         if (statusId == null) {
             return null;
         }
+        TwitterConfig config = new TwitterConfig.Builder(context)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET))
+                .debug(true)
+                .build();
 
-        Twitter.initialize(context);
+        Twitter.initialize(config);
 
         TwitterApiClient client = TwitterCore.getInstance().getApiClient();
         StatusesService service = client.getStatusesService();
